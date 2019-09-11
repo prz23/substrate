d57e53ec3aa101f322f18ce4b44fed879e7a0cfa
prz23 <panruizheng@zju.edu.cn>
2019-09-11 19:36:10 +0800

    remove gensis args

--- srml/libra/src/mock.rs
+++ srml/libra/src/mock.rs
@@ -75,10 +75,12 @@ impl Trait for Test {
 
 pub fn new_test_ext(authorities: Vec<u64>) -> runtime_io::TestExternalities<Blake2Hasher> {
 	let mut t = system::GenesisConfig::default().build_storage::<Test>().unwrap();
+	/*
 	GenesisConfig::<Test>{
 		authorities: authorities.into_iter().map(|a| UintAuthorityId(a).to_public_key()).collect(),
 	}.assimilate_storage(&mut t).unwrap();
 	t.into()
+	*/
 }
 
 pub type Aura = Module<Test>;
